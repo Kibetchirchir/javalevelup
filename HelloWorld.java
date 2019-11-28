602749a64e5bf56823d5d0d881ee403d9cb4b072
chirchir <langatchirchir@gmail.com>
2019-11-28 16:39:07 +0200

    story(method): overload a method
    - implement overloading a method
    
    [Finishes]

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,8 +8,19 @@ public class HelloWorld{
         }
         System.out.println(name + " is " + status);
     }
+    public static int ageIn5yrs(int age){
+        int ageIN5 = age + 5;
+        return ageIN5;
+    }
+    public static String ageIn5yrs(double age){
+        String error = "invalid age meaning ageIn5yrs was overloaded";
+        return error;
+    }
     public static void main(String[] args) {
         methodExample("Chirchir", 22);
         methodExample("Kenneth", 15);
+        System.out.println(ageIn5yrs(5));
+        System.out.println(ageIn5yrs(5.5));
+
     }
 }
