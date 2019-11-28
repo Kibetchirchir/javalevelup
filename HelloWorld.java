e1c53db18f48bdebfee0396475d306f815861a27
chirchir <langatchirchir@gmail.com>
2019-11-28 16:12:10 +0200

    story(method): implement args on method
    - create method with arg
    
    [Finishes]

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,15 @@
 public class HelloWorld{
-    public static void methodExample(String name) {
-        System.out.println("His name is " + name);
+    public static void methodExample(String name, int age) {
+        String status;   
+        if(age < 18){
+            status = "a Child";
+        }else{
+            status = "an Adult";
+        }
+        System.out.println(name + " is " + status);
     }
     public static void main(String[] args) {
-        methodExample("Chirchir");
-        methodExample("Kenneth");
+        methodExample("Chirchir", 22);
+        methodExample("Kenneth", 15);
     }
 }
