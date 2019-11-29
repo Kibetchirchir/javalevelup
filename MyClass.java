a8a8cf3b3229c91cf7a2740decfb2882bf3809b1
chirchir <langatchirchir@gmail.com>
2019-11-29 11:34:39 +0200

    story(class): implement class
    - implement class
    - implement object
    
    [FInishes]

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+class MyClass{
+    int x;
+
+    public static void main(String[] args) {
+        MyClass newObj = new MyClass();
+        newObj.x = 3;
+        System.out.println(newObj.x);
+    }
+}
