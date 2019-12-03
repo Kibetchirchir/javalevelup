df6b472377f7f67e2f39d39170bfad4edf7859d3
chirchir <langatchirchir@gmail.com>
2019-12-03 17:56:51 +0200

    story(polymophosim): implements polymophosm
    - adds polymphosim
    
    [Finishes]

--- Car.java
+++ Car.java
@@ -1,11 +1,13 @@
 class Car extends Vehicle{ 
+    public void accelerate(){
+        System.out.println("vuum like a subaru");
+    }
     public static void main(String[] args) {
         Car car = new Car();
         car.accelerate();
         car.ignite();
     }
 }
-
 class Vehicle{
     public void accelerate(){
         System.out.println("vuum");
